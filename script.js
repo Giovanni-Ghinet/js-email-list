51d79565d2616d3c9836f02202802919de84ce60
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-15 16:04:57 +0200

    stampato in console 10 mail diverse

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ API_URL_RANDOM_WORD = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const emailListHtml = document.querySelector('#EmailList');
 
+for (let i = 0; i < 10; i++) {
 fetch(API_URL_RANDOM_WORD)
 .then((risposta) => {
-    console.log(risposta);
     return risposta.json();
 })
+
 .then(function(jsonData) {
     console.log(jsonData);
-    
-    const emails = jsonData.results;
 
-    const emailListInnerHtml = emails.map(email => {
-        return `<div>${email.name}</div>`
-    });
-    emailListHtml.innerHTML = emailListInnerHtml.join("");
+    const emails = jsonData.response;
+        console.log(emails);
+
 });
+}
