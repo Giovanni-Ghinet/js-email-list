43b7e699d88930df20e8f7e43d35fb40b25ca4af
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-15 16:22:18 +0200

    aggiunto controllo che quando sono a 10 mail parte la funzione che mi crea i 10 li e inserisce la mail dall'array che ho riempito di mail create daal fetch

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ API_URL_RANDOM_WORD = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const emailListHtml = document.querySelector('#EmailList');
 
+let emailsArray = [];
+
+function updateEmailList() {
+    // Svuoto la lista corrente
+    emailListHtml.innerHTML = '';
+    
+    // Inserisco tutte le email nella lista
+    emailsArray.forEach(email => {
+        const li = document.createElement('li');
+        li.textContent = email;
+        emailListHtml.appendChild(li);
+    });
+}
+
 for (let i = 0; i < 10; i++) {
 fetch(API_URL_RANDOM_WORD)
 .then((risposta) => {
@@ -13,6 +27,13 @@ fetch(API_URL_RANDOM_WORD)
 
     const emails = jsonData.response;
         console.log(emails);
+    
+    emailsArray.push(emails);
+
+    // Quando abbiamo raccolto tutte e 10 le email, aggiorno la lista
+    if (emailsArray.length === 10) {
+    updateEmailList();
+    }
 
 });
 }
